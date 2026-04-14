03a9c2f992a3bd0a16243ce8732344071e7f61f2
Themba S'phile Sibeko <166844390+SibekoThemba@users.noreply.github.com>
2026-04-14 19:37:04 +0200

    Move
    Changed location of the file

--- src/test/java/TestLogins/LoginTest.java
+++ src/test/java/TestLogins/LoginTest.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-package com.mycompany.prog1a.whatsapp;
+package TestLogins;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
